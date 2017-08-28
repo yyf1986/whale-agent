cb81b9a9a347431fa8b5f7c248ad8504dcbb7798
yyf1986 <dn-yyf@163.com>
2017-08-28 22:39:00 +0800

    aa

--- controllers/res.go
+++ controllers/res.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"github.com/astaxie/beego"
 	"whale-agent/models"
+	"strconv"
 )
 
 type ResController struct {
@@ -16,6 +17,14 @@ func (d *ResController) Get() {
 
 func (d *ResController) GetPort() {
 	d.Data["json"] = map[string]int{"port": models.GetPort()}
+	d.ServeJSON()
+}
+
+func (d *ResController) DelPort() {
+	p := d.GetString("port")
+	port, _ := strconv.Atoi(p)
+	models.DelPort(port)
+	d.Data["json"] = map[string]int{"status": 200}
 	d.ServeJSON()
 }
 
--- routers/router.go
+++ routers/router.go
@@ -14,12 +14,13 @@ import (
 )
 
 func init() {
-	//beego.Router("/", &controllers.ContainerController{}, "Get:Index")
-	//beego.Router("/v1/container/create", &controllers.ContainerController{}, "Get:Create")
-	//beego.Router("/v1/container/start", &controllers.ContainerController{}, "Get:Start")
-	//beego.Router("/v1/container/stop", &controllers.ContainerController{}, "Get:Stop")
-	//beego.Router("/v1/container/del", &controllers.ContainerController{}, "Get:Del")
-	//beego.Router("/v1/container/getall", &controllers.ContainerController{}, "Get:GetAll")
+	beego.Router("/", &controllers.ContainerController{}, "Get:Index")
+	beego.Router("/v1/container/create", &controllers.ContainerController{}, "Get:Create")
+	beego.Router("/v1/container/start", &controllers.ContainerController{}, "Get:Start")
+	beego.Router("/v1/container/stop", &controllers.ContainerController{}, "Get:Stop")
+	beego.Router("/v1/container/del", &controllers.ContainerController{}, "Get:Del")
+	beego.Router("/v1/container/getall", &controllers.ContainerController{}, "Get:GetAll")
 	beego.Router("/v1/res/host", &controllers.ResController{})
 	beego.Router("/v1/res/getport", &controllers.ResController{}, "Get:GetPort")
+	beego.Router("/v1/res/delport", &controllers.ResController{}, "Get:DelPort")
 }
