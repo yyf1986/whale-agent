4071ee92efe660b2b107c8320ea811bbb7369651
yyf1986 <dn-yyf@163.com>
2017-08-28 12:46:54 +0800

    up

--- routers/router.go
+++ routers/router.go
@@ -14,12 +14,11 @@ import (
 )
 
 func init() {
-	beego.Router("/", &controllers.DockerController{}, "Get:Index")
-	beego.Router("/v1/container/create", &controllers.DockerController{}, "Get:Create")
-	beego.Router("/v1/container/start", &controllers.DockerController{}, "Get:Start")
-	beego.Router("/v1/container/stop", &controllers.DockerController{}, "Get:Stop")
-	beego.Router("/v1/container/del", &controllers.DockerController{}, "Get:Del")
-	beego.Router("/v1/container/getall", &controllers.DockerController{}, "Get:GetAll")
+	beego.Router("/", &controllers.ContainerController{}, "Get:Index")
+	beego.Router("/v1/container/create", &controllers.ContainerController{}, "Get:Create")
+	beego.Router("/v1/container/start", &controllers.ContainerController{}, "Get:Start")
+	beego.Router("/v1/container/stop", &controllers.ContainerController{}, "Get:Stop")
+	beego.Router("/v1/container/del", &controllers.ContainerController{}, "Get:Del")
+	beego.Router("/v1/container/getall", &controllers.ContainerController{}, "Get:GetAll")
 	beego.Router("/v1/host/res", &controllers.ResController{})
-	beego.Router("/v1/host/res2", &controllers.ResController{},"Get:Get2")
 }
